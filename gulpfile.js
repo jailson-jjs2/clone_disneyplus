38226d623fcda7b46e18ccc528744b13d2e8a7c1
Jailson Joventino <jjs2@hotmail.com>
2023-04-27 17:28:07 -0300

    setup do projeto

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+/* TESTANDO O GULP
+
+function testeGulp(teste) {
+  console.log("testando gulp");
+  teste();
+}
+
+exports.default = testeGulp; */
+
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function styles() {
+  return gulp.src('./src/styles/*.scss')
+  .pipe(sass({outputStyle: 'compressed'})) /* PARA COMPRIMIR */
+  .pipe(gulp.dest('./dist/css'));
+}
+
+exports.default = styles;
+exports.watch = function() {
+  gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+}
