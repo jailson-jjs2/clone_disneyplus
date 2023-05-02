3b6e8f6f25d8b59a25e4d0f90de50c90691636fa
Jailson Joventino <jjs2@hotmail.com>
2023-05-02 16:19:08 -0300

    Concluido Projeto

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,13 @@ exports.default = testeGulp; */
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
+function scripts() {
+  return gulp.src('./src/scripts/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('./dist/js'));
+}
 
 function styles() {
   return gulp.src('./src/styles/*.scss')
@@ -24,9 +30,10 @@ function images() {
     .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);/* AS FUNÇÕES */
 
 exports.watch = function() {
   gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+  gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
 
